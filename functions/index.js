cc2e063b3218f429188b2846b9bd3a03ad4fbe07
unknown <bhavya.saggi@gmail.com>
2020-12-01 12:14:09 +0530

    init

--- functions/index.js
+++ functions/index.js
@@ -4,26 +4,8 @@ const fetch = require('cross-fetch')
 const cors = require('cors')({ origin: true })
 const base64 = require('base-64')
 
-const validateFirebaseIdToken = async (request, response, next) => {
-  let idToken
-  if (
-    request.headers.authorization &&
-    request.headers.authorization.startsWith('Bearer ')
-  ) {
-    idToken = request.headers.authorization.split('Bearer ')[1]
-  } else if (request.cookies && request.cookies.__session) {
-    idToken = request.cookies.__session
-  } else {
-    next(new Error('Unauthorized'))
-  }
-  try {
-    const decodedIdToken = await admin.auth().verifyIdToken(idToken)
-    request.user = decodedIdToken
-  } catch (e) {
-    next(e)
-  }
-  next()
-}
+// INIT CLOUD FUNCTION
+admin.initializeApp()
 
 // Secrets
 const {
@@ -105,21 +87,46 @@ const PABBLY_API_VERIFYHOSTED = async (hostedpage) => {
   return resBody
 }
 
-// INIT CLOUD FUNCTION
-admin.initializeApp()
+const validateFirebaseIdToken = async (request, response, next) => {
+  let idToken
+  if (
+    request.headers.authorization &&
+    request.headers.authorization.startsWith('Bearer ')
+  ) {
+    idToken = request.headers.authorization.split('Bearer ')[1]
+  } else if (request.cookies && request.cookies.__session) {
+    idToken = request.cookies.__session
+  } else {
+    next(new Error('Unauthorized'))
+  }
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken)
+    request.user = decodedIdToken
+  } catch (e) {
+    next(e)
+  }
+  next()
+}
+
+const customerToUserId = async (customer_id) => {
+  const userData = await admin
+    .database()
+    .ref('users')
+    .orderByChild('customer_id')
+    .equalTo(customer_id)
+    .once('value')
+  const userValue = userData.val()
+  if (!userValue || !Object.keys(userValue).length) {
+    return null
+  }
+  return Object.keys(userValue)[0]
+}
 
 const syncUser = async (uid, customClaims = {}) => {
   // Set customerId in customerClaim
   await admin
     .auth()
     .setCustomUserClaims(uid, Object.assign({}, customClaims || {}))
-  // Set customerId-uid in Realtime Database
-  if (customClaims.customer_id) {
-    await admin
-      .database()
-      .ref('ccLookup/' + customClaims.customer_id)
-      .set(uid)
-  }
   // Set Shared Claims in Realtime Database
   const shareClaims = ['customer_id', 'plan_id', 'product_id']
   for (let i = 0; i < shareClaims.length; i += 1) {
@@ -156,6 +163,83 @@ const syncUser = async (uid, customClaims = {}) => {
 //     return
 //   })
 
+//TODO: Pabbly webhook Events
+exports.pabblyEvent = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    // TODO: Add Basic Authentication (same username/password as pabbly)
+    try {
+      const {
+        event_type: eventType,
+        event_source: eventSource,
+        data,
+      } = request.body
+      switch (eventType) {
+        case 'customer_create':
+          // TODO: Anonymous Checkouts
+          // Check if email exists
+          // if yes: then Sync
+          // if no: then Create Related new Anonymous User
+          break
+        case 'customer_delete':
+          break
+        case 'subscription_activate':
+          break
+        case 'subscription_create':
+          break
+        case 'subscription_upgrade':
+          break
+        case 'subscription_cancel':
+          break
+
+        case 'payment_success':
+          let user = null
+          const { customer_id, plan_id, product_id } = data || {}
+          if (!customer_id && !plan_id && !product_id) {
+            throw new Error('Invalid Request.')
+          }
+
+          // fetch uid from firebase realtime database & user from firebase Auth
+          if (!user && customer_id) {
+            const lookupId = await customerToUserId(customer_id)
+            // fetch user from firebase auth
+            if (lookupId) {
+              user = await admin.auth().getUser(lookupId)
+            }
+          }
+
+          // Create anonymous User
+          if (!user) {
+            user = await admin.auth().createUser({
+              displayName,
+              email,
+              emailVerified: false,
+            })
+            user.sendEmailVerification()
+          }
+
+          // Sync User customClaim
+          const updatedClaims = Object.assign({}, user.customClaims || {}, {
+            customer_id,
+            plan_id,
+            product_id,
+          })
+          await syncUser(user.uid, updatedClaims)
+          break
+
+        case 'invoice_create':
+          break
+        default:
+          break
+      }
+      response.json({ status: 'success' })
+    } catch (e) {
+      response.status(500).send(e.toString())
+    }
+    return
+  })
+})
+
+// Pabbly Pre-Checkout Verifier | Integrator
 exports.userSync = functions.https.onRequest((request, response) => {
   cors(request, response, async () => {
     validateFirebaseIdToken(request, response, async (error) => {
@@ -208,92 +292,6 @@ exports.userSync = functions.https.onRequest((request, response) => {
   })
 })
 
-// TODO: REMOVE DEBUG
-// exports.userAll = functions.https.onRequest(async (request, response) => {
-//   const allUsers = await admin.auth().listUsers()
-//   response.json(allUsers)
-// })
-
-//TODO: Pabbly webhook Events
-exports.pabblyEvent = functions.https.onRequest((request, response) => {
-  cors(request, response, async () => {
-    // TODO: Add Basic Authentication (same username/password as pabbly)
-    try {
-      const {
-        event_type: eventType,
-        event_source: eventSource,
-        data,
-      } = request.body
-      switch (eventType) {
-        case 'customer_create':
-          // TODO: Anonymous Checkouts
-          // Check if email exists
-          // if yes: then Sync
-          // if no: then Create Related new Anonymous User
-          break
-        case 'customer_delete':
-          break
-        case 'subscription_activate':
-          break
-        case 'subscription_create':
-          break
-        case 'subscription_upgrade':
-          break
-        case 'subscription_cancel':
-          break
-
-        case 'payment_success':
-          let user = null
-          const { customer_id, plan_id, product_id } = data || {}
-          if (!customer_id && !plan_id && !product_id) {
-            throw new Error('Invalid Request.')
-          }
-
-          // fetch uid from firebase realtime database & user from firebase Auth
-          if (!user && customer_id) {
-            const lookupSnap = await admin
-              .database()
-              .ref('ccLookup/' + customer_id)
-              .once('value')
-            const lookupId = lookupSnap.val()
-            // fetch user from firebase auth
-            if (lookupId) {
-              user = await admin.auth().getUser(lookupId)
-            }
-          }
-
-          // Create anonymous User
-          if (!user) {
-            user = await admin.auth().createUser({
-              displayName,
-              email,
-              emailVerified: false,
-            })
-            user.sendEmailVerification()
-          }
-
-          // Sync User customClaim
-          const updatedClaims = Object.assign({}, user.customClaims || {}, {
-            customer_id,
-            plan_id,
-            product_id,
-          })
-          await syncUser(user.uid, updatedClaims)
-          break
-
-        case 'invoice_create':
-          break
-        default:
-          break
-      }
-      response.json({ status: 'success' })
-    } catch (e) {
-      response.status(500).send(e.toString())
-    }
-    return
-  })
-})
-
 // Pabbly Checkout Landing Page Verifier | Integrator
 exports.payValidate = functions.https.onRequest((request, response) => {
   cors(request, response, async () => {
@@ -311,11 +309,7 @@ exports.payValidate = functions.https.onRequest((request, response) => {
       } = await PABBLY_API_VERIFYHOSTED(hostedpage)
 
       // Get firebase auth uid using customer_id from realtime database
-      const lookupSnap = await admin
-        .database()
-        .ref('ccLookup/' + customer_id)
-        .once('value')
-      const lookupId = lookupSnap.val()
+      const lookupId = await customerToUserId(customer_id)
 
       if (lookupId) {
         // fetch user from firebase auth
@@ -351,5 +345,35 @@ exports.payValidate = functions.https.onRequest((request, response) => {
       response.status(500).json(e.toString())
     }
     return
+  })
+})
+
+exports.customSite = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    validateFirebaseIdToken(request, response, async (error) => {
+      try {
+        if (error instanceof Error) {
+          throw error
+        }
+        // Get User from authentication
+        // Get User customer_id & plan_id
+        // Get websiteKey from request
+        // Get Website Details from websiteKey
+        // Get Customer Active Plans from Pabbly
+        // Sync customer_id & plan_id
+        // Verify if plan is active
+        // Fetch All assets from firebase bucket at websiteKey
+        // Refer: https://docs.aws.amazon.com/AmazonS3/latest/dev/HostingWebsiteOnS3Setup.html
+        // Create new S3 Bucket
+        // Clear `Block all public access`
+        // Attach a bucket policy
+        // Push All assets at S3 Bucket
+        // Create Route53
+        // Point to S3 Bucket
+        // return NS from Route 53
+      } catch (e) {
+        response.status(500).json(e.toString())
+      }
+    })
   })
 })
