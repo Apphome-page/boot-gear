3777c0c6ae1e31e2bd7501aa3f32702a8f3851ae
Harshit-Raghav21 <59803519+Harshit-Raghav21@users.noreply.github.com>
2020-12-04 15:44:24 +0530

    Update index.js

--- pages/app-screenshot-generator/index.js
+++ pages/app-screenshot-generator/index.js
@@ -7,19 +7,24 @@ import MockUp from '../../pageComponents/MockUp'
 
 const faqList = [
   {
-    title: 'App Screenshot Generator',
+    title: 'What is an app screenshot?',
     desc:
-      'Best screenshot generator for your iPhone and Android apps.  Use it for free without login in.',
+      'App screenshots have two simple jobs: grab the attention of the potential customer and convince them to download the app',
   },
   {
-    title: 'App Screenshot Generator',
+    title: 'How to use Applanding.page screenshot generator?',
     desc:
-      'Best screenshot generator for your iPhone and Android apps.  Use it for free without login in.',
+      'Drag a screenshot from your desktop(in PNG format). Customize the generated image and drag it to your desktop to save. Use multiple theme and templates for free and enhance your playstore listings.',
   },
   {
-    title: 'App Screenshot Generator',
+    title: 'What are key highlights of Applanding page’s screenshot generator?',
     desc:
-      'Best screenshot generator for your iPhone and Android apps.  Use it for free without login in.',
+      'Applanding page\'s App Screenshot generator is really easy to use. You can generate Screenshot that are required for iPhone and Android app with a single click. <br> Applanding.page\'s Screenshot generator does not send any data to server, hence is it fully secure and your data does not leave your browser. <br>No hidden cost or fee. <br>Select from pre designed dozens of templates and create your own trendy screenshot. <br>Export in all sizes for Google Play and Apple store. <br>Designer look for screenshots in one click.',
+  },
+  {
+    title: 'How to use Applanding.page screenshot generator?',
+    desc:
+      'Drag a screenshot from your desktop(in PNG format). Customize the generated image and drag it to your desktop to save. Use multiple theme and templates for free and enhance your playstore listings.',
   },
 ]
 
