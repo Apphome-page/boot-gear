cabad9b63e1b69a95bf876e1da71de89282e4f60
Harshit-Raghav21 <59803519+Harshit-Raghav21@users.noreply.github.com>
2020-12-04 15:06:31 +0530

    Update index.js

--- pages/android-app-icon-generator/index.js
+++ pages/android-app-icon-generator/index.js
@@ -9,7 +9,7 @@ const faqList = [
   {
     title: 'How to generate app icons for android ?',
     desc:
-      'Using Applanding.page, just drag or upload an app icon image to generate app icon for android.',
+      'Using Applanding.page, just drag or upload an app icon image to generate app icon for android mdpi, hdpi, xhdpi, xxhdpi, xxxhdpi..',
   },
   {
     title: 'How much time is requied to generate app icon for android ?',
@@ -20,6 +20,11 @@ const faqList = [
     title: 'What is the easiest way to generate app icons for android',
     desc:
       'Just with a few clicks you can create app icons for android using Applandingpage.',
+  },
+  {
+    title: 'What is the USP of Applanding page’s Android app icon generator?',
+    desc:
+      'Applanding page\'s Android icon generator is really easy to use. You can generate icons that are required for Android with  a single click. Applanding.page\'s icon generator does not send any data to server, hence is it fully secure and your data does not leave your browser. No hidden cost or fee',
   },
 ]
 
