d0229b2c916a25cb36ee5326f5692355a56b10de
unknown <bhavya.saggi@gmail.com>
2021-04-02 19:23:41 +0530

    amp build fail fix

--- pages/home.js
+++ pages/home.js
@@ -9,7 +9,7 @@ import HomeFeatures from '../pageComponents/Home/features'
 import HomeBlog from '../pageComponents/Home/blog'
 
 // TODO: Convert home to AMP
-export const config = { amp: true }
+// export const config = { amp: true }
 
 export default function Home() {
   return (
@@ -18,9 +18,6 @@ export default function Home() {
         <title>
           App landing page for mobile apps, with free screenshots tools
         </title>
-        {
-          // TODO: Remove this after successful AMP Migration
-        }
         <meta name='robots' content='noindex' />
       </Head>
       <HomeHero />
